305d718a4e0a57eb8a6209143e2c01ad1b339c37
Oleksandr Lisunov <liso@ciklum.com>
2022-01-10 17:58:10 +0200

    changes in js for edit btn

--- scripts/config.js
+++ scripts/config.js
@@ -2,3 +2,12 @@ function openPlayerConfig() {
     playerConfigOverlayElement.style.display = 'block';
     backdropElement.style.display = 'block';
 }
+
+function closePlayerConfig() {
+    playerConfigOverlayElement.style.display = 'none';
+    backdropElement.style.display = 'none';
+}
+
+function savePlayerConfig() {
+    
+}
